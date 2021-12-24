94028dd6e0757e18545642538d43c7bcdcb8668a
vnillaDev <juzen.kiendler@gmail.com>
2021-12-24 16:15:40 +0100

    added AppBar and Body

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
+/*
 void main() {
   runApp(MyApp());
 }
+*/
+
+void main() =>
+    runApp(MyApp()); // valid Dart syntax for Funtions with ONE expression!
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      // ctrl + 'space' for code suggestions
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Text('This is a default Text'),
+      ),
     );
   }
 }
+
+// **Tabs**
 // Video Timestamp: https://youtu.be/x0uinJvhNxI?t=9595
+// Dartpad
 // Flutter Repo auf GitHub
 // Markdown Docomentation
