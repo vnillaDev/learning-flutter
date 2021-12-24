ec7e6c25b77ef858bebc6ff470e53bf3926ddaa0
vnillaDev <juzen.kiendler@gmail.com>
2021-12-24 18:05:02 +0100

    made buttons work

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -6,26 +6,71 @@ void main() {
 }
 */
 
-void main() =>
-    runApp(MyApp()); // valid Dart syntax for Funtions with ONE expression!
+void main() => runApp(
+    const MyApp()); // valid Dart syntax for Funtions with ONE expression!
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestions() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s favorite food?',
+    ];
+
     return MaterialApp(
       // ctrl + 'space' for code suggestions
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My first App'),
+          title: const Text('My first App'),
         ),
-        body: Text('This is a default Text'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              child: const Text('Answer 1'),
+              onPressed: answerQuestions, // passing a Pointer to onPressed
+            ),
+            ElevatedButton(
+              child: const Text('Answer 2'),
+              onPressed: () =>
+                  print('answer 2 chosen!'), // single-line anonymous functions
+            ),
+            ElevatedButton(
+              child: const Text('Answer 3'),
+              onPressed: () {
+                // anonymous function with more than one line
+                print('answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 // **Tabs**
-// Video Timestamp: https://youtu.be/x0uinJvhNxI?t=9595
+// Video Timestamp: https://youtu.be/x0uinJvhNxI?t=13116
 // Dartpad
 // Flutter Repo auf GitHub
 // Markdown Docomentation
